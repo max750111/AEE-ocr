5959da69516500bc57a57dc80ef6a8eb5aa6fad3
Max Chuang <hellomax111@gmail.com>
2021-06-11 22:55:58 +0800

    almost done

--- ocr.py
+++ ocr.py
@@ -1,7 +1,7 @@
 import pytesseract
 import cv2
-import numpy as np
 from os import listdir
+import pyperclip
 
 
 def resize_image(frame):
@@ -36,7 +36,7 @@ def save_image(image, num):
 def load_film(film_filename):
     video_capture = cv2.VideoCapture(film_filename)
 
-    per_frame = 7  # 設定多少幀截圖
+    per_frame = 5  # 設定多少幀截圖
     i = 0
     j = 0
     while True:
@@ -45,7 +45,7 @@ def load_film(film_filename):
             i = i + 1
             if i % per_frame == 0:
                 j = j + 1
-                save_image(resize_image(frame), '{0:05d}'.format(j))
+                save_image(frame, '{0:05d}'.format(j))
                 print('save image:', j)
         elif success == 0:
             break
@@ -55,7 +55,7 @@ def load_film(film_filename):
 # 讀取所有放在image資料夾待辨識圖片檔檔名，存在queue_job列表
 def queue_img(path):
     files = listdir(path)
-    return sorted(files, key = lambda x : int(x[-9:-4]))  # list
+    return files  # list
 
 
 # 黑白反轉，增加辨識準確率，轉換結果放到result資料夾
@@ -64,44 +64,48 @@ def change_color_crop(image):
         img_origin = cv2.imread("./output/" + img, 1)
         gray = cv2.cvtColor(img_origin, cv2.COLOR_BGR2GRAY)
         img_result = 255 - gray
-        crop_img = img_result[170:890, 70:770]
+        crop_img = img_result[230:1100, 80:980]
         cv2.imwrite("./result/" + img, crop_img)
 
 
 def all_name(transcript_list):
     for index, str in enumerate(transcript_list):
         if str.startswith("M:"):
-            transcript_list[index] = transcript_list[index].replace("M:", "FuckM")
+            transcript_list[index] = transcript_list[index].replace("M:", "\nMichelle:")
         elif str.startswith("L:"):
-            transcript_list[index] = transcript_list[index].replace("L:", "FuckL")
+            transcript_list[index] = transcript_list[index].replace("L:", "\nLindsay:")
+
+
+transcript = []
 
 
 # 文字辨識(未完成)
 def ocr():
-    transcript = []
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-    for img_file in queue_img("./result"):
+    for img_file in sorted(queue_img("./result"), key=lambda x: int(x[-9:-4])):
         print(img_file)
         img_for_ocr = cv2.imread("./result/" + img_file)
-        a = pytesseract.image_to_string(img_for_ocr, lang="eng")
+        a = pytesseract.image_to_string(img_for_ocr, lang="eng", config="--oem 1 --psm 6")
         a = a.strip()  # 去除最後的空白字元
         ocr_result = a.split("\n")  # 用\n分割每行，存成列表
+        ocr_result = [x for x in ocr_result if x != ' ']
         print(ocr_result)
-        for str in ocr_result[1:-2]:
-            if str not in transcript[-11:0]:
+        for str in ocr_result[2:-4]:
+            if str not in transcript[-11:]:
                 transcript.append(str)
     print(transcript)
-    #all_name(transcript)
-    #transcript_final = " ".join(transcript)
-
+    all_name(transcript)
+    transcript_final = " ".join(transcript)
     #print(transcript_final)
+
+    pyperclip.copy(transcript_final)
 
 
 def main():
-    #pass
+    # pass
     load_film('1.mp4')
     change_color_crop(queue_img("./output"))
-    #ocr()
+    ocr()
 
 
 if __name__ == "__main__":
