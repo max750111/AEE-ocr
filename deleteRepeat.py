5623faa8370446344548fc087735cdf9ff240e62
max750111 <hellomax111@gmail.com>
2021-06-16 16:54:27 +0800

    test delete funtion

--- deleteRepeat.py
+++ deleteRepeat.py
@@ -1,16 +1,5 @@
-temp_list = ["L: | think I'm going to get", 'back into it. | think in June,', ]
-transcript = ["L: I think I'm going to get", 'back into it. | think in June,',]
+import difflib
+str1 = "have to drag you kicking and"
+str2 = "to drag me kicking and"
 
-
-for i in temp_list:
-    c = 0
-    for x in i.split():
-        print(i.split())
-        for j in transcript[-15:]:
-            for y in j.split():
-                print(j.split())
-                if x == y:
-                    c += 1
-    if c <= 5:
-        transcript.append(i)
-print(transcript)
+print(difflib.SequenceMatcher(None, str1, str2).quick_ratio())
