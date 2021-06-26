a9006badaca982f154e78a9948d23ccde3c727b3
Max Chuang <hellomax111@gmail.com>
2021-06-27 00:01:53 +0800

    adjust doc save

--- doc.py
+++ doc.py
@@ -1,8 +1,9 @@
 from docx import Document
 import pyperclip
 
+v = '1895.mp4'
 print(pyperclip.paste())
 doc = Document()
 
 doc.add_paragraph(pyperclip.paste())
-doc.save('demo.docx')
+doc.save('./ocr _final/' + v[:-4] + '.docx')
