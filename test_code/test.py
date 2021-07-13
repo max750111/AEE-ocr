649b9669b20d0e7e6c9deba43d74a72f57f75006
max750111 <hellomax111@gmail.com>
2021-07-13 17:31:40 +0800

    classify episode

--- test_code/test.py
+++ test_code/test.py
@@ -9,6 +9,8 @@ def queue_img(path):
 
 episode = dict()
 episode_num = re.compile(r'AEE (\d*)')
+delete_file = set()
+print(type(delete_file))
 
 for i in queue_img("../output"):
     pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
@@ -18,11 +20,15 @@ for i in queue_img("../output"):
     a = pytesseract.image_to_string(img_gray[1200:1650, 80:980], lang="eng",
                                 config="--oem 1 --psm 6")
     m = episode_num.search(a)
-    if m.group(1) not in episode:
-        episode[m.group(1)] = []
-        episode[m.group(1)].append(i)
-        print(episode)
+    if m:
+        if m.group(1) not in episode:
+            episode[m.group(1)] = []
+            episode[m.group(1)].append(i)
+            print(episode)
+        else:
+            episode[m.group(1)].append(i)
+            print(episode)
     else:
-        episode[m.group(1)].append(i)
-        print(episode)
-print(episode)
+        delete_file.add(i)
+        print(delete_file)
+
