1a9a4f3316e02387b2e2c26a360e92ab4a724838
max750111 <hellomax111@gmail.com>
2021-07-13 16:30:38 +0800

    ocr reform version(working)

--- test_code/test.py
+++ test_code/test.py
@@ -1,8 +1,28 @@
-import multiprocessing
-cpus = multiprocessing.cpu_count()
+import pytesseract
+import cv2
+import re
+from os import listdir
 
-print(cpus)
+def queue_img(path):
+    files = listdir(path)
+    return files  # list
 
-list1 = [1,2,3]
-a = list1.append(3)
-print(a)
+episode = dict()
+episode_num = re.compile(r'AEE (\d*)')
+
+for i in queue_img("../output"):
+    pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+    img_for_ocr = cv2.imread("../output/" + i, cv2.IMREAD_GRAYSCALE)
+    img_gray = 255 - img_for_ocr
+    a = pytesseract.image_to_string(img_gray[1200:1650, 80:980], lang="eng",
+                                config="--oem 1 --psm 6")
+    m = episode_num.search(a)
+    if m.group(1) not in episode:
+        episode[m.group(1)] = []
+        episode[m.group(1)].append(i)
+        print(episode)
+    else:
+        episode[m.group(1)].append(i)
+        print(episode)
+print(episode)
